f214d46e43bde72ba489c1753c98195e78fa76f1
rolando22 <rolando2207@gmail.com>
2023-06-19 20:48:38 -0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export function App() {
+    return (
+        <>
+            <header>
+                <h1>Buscador de Péliculas</h1>
+            </header>
+        </>
+    );
+}

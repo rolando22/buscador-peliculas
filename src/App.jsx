373e3839f6ee92fb6e6485701fe768a87cfc85c8
rolando22 <rolando2207@gmail.com>
2023-06-22 18:01:03 -0300

    [ADD] render inicial de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import movies from './mocks/okResponse.json';
+
 import './App.css';
 
 export function App() {
     return (
         <>
-            <header>
+            <header className='App-header'>
                 <h1>Buscador de Péliculas</h1>
+                <form className='App-form'>
+                    <input type='text' placeholder='Avengers, Spider-Man, Matrix...' />
+                    <button>Buscar</button>
+                </form>
             </header>
+            <main>
+                <ul className='App-list'>
+                    {movies.Search.map(movie => 
+                        <li key={movie.imdbID}>
+                            <h3>{movie.Title}</h3>
+                            <p>{movie.Year}</p>
+                            <img src={movie.Poster} alt={movie.Title} />
+                        </li>
+                    )}
+                </ul>
+            </main>
         </>
     );
 }

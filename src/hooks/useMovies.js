41129836ec3d3c050d42f8b53ecf5efc2aad5ba6
rolando22 <rolando2207@gmail.com>
2023-06-24 21:30:38 -0300

    [ADD] evitar renderizar al hacer la misma búsqueda

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { searchMovies } from "../services/movies.service";
 
 export function useMovies({ search }) {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const previousSearch = useRef(search);
 
     const getMovies = async () => {
+        if (search === previousSearch.current) return;
         try {
             setLoading(true);
             setError(null);
+            previousSearch.current = search;
             const searchedMovies = await searchMovies({ search });
             setMovies(searchedMovies);
         } catch (error) {
